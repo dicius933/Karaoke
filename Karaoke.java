29f3fe7ccb7da7f8258b7bd2e13e4efc730bcc95
dicius933 <dicius933@gmail.com>
2017-09-16 12:56:11 -0400

    Initial commit

---
+++ Karaoke.java
@@ -0,0 +1,14 @@
+import com.teamtreehouse.model.Song;
+import com.teamtreehouse.model.SongBook; 
+import com.teamtreehouse.KaraokeMachine;
+
+public class Karaoke{
+  public static void main(String[] args){
+    SongBook songBook = new SongBook();
+    songBook.importFrom("songs.txt");
+    KaraokeMachine machine = new KaraokeMachine(songBook);
+    machine.run();
+    System.out.println("Saving book...");
+    songBook.exportTo("songs.txt"); 
+  }
+}
